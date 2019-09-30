294f2ba94a74460d84f549093d0a2bb5c53f3491
Linh Ngo <linh@turncommerce.com>
2019-09-30 16:34:26 -0600

    Initial check in

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+/// <summary>
+/// Example showing conditional compilation taken from:
+/// https://blog.magnusmontin.net/2018/11/05/platform-conditional-compilation-in-net-core
+/// </summary>
+namespace platformconditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                    Console.WriteLine("Running on Linux!");
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+                    Console.WriteLine("Running on macOS!");
+                else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                    Console.WriteLine("Running on Windows!");
+
+#if Linux
+        Console.WriteLine("Built on Linux!"); 
+#elif OSX
+        Console.WriteLine("Built on macOS!"); 
+#elif Windows
+        Console.WriteLine("Built in Windows!"); 
+#endif
+            }
+        }
+    }
+}
